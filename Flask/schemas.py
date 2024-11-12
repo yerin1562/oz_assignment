e6d3900bd3cf3dc9ae04c82357207187548f304e
yeringwak <yerin1562@gmail.com>
2024-11-12 16:42:36 +0900

    "ORM"

--- Flask/schemas.py
+++
@@ -1,8 +0,0 @@
-# Marshmellow 모듈로 데이터 타입 바꾸기
-from marshmallow import Schema, fields
-
-
-class BookSchema(Schema):
-    id = fields.Int(dump_only=True)
-    title = fields.String(required=True)
-    author = fields.String(required=True)
